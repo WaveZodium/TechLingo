2e741bb4893e1c1a67ef873e32c294319231bb12
EmilAlexLarsson <emilalexlarsson@gmail.com>
2026-09-02 17:51:16 +0200

    feat: add Iridescence component with shaders and styles; update Layout to include Iridescence background

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,13 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import "../../styles/Layout.css";
+import Iridescence from "../iridescence/Iridescence";
+
 
 function Layout() {
   return (
     <div className="layout">
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
+      <div className="layout-background">
+        <Iridescence
+          color={[0.0039, 0.0863, 0.2157]}
+          mouseReact={false}
+          amplitude={0.1}
+          speed={1}
+        />
+      </div>
+
+      <div className="layout-content">
+        <Navbar />
+
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
