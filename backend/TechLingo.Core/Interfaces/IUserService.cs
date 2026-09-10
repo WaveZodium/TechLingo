29a0cc2bdb9e9613392967dbf254d882b38d86d1
EmilAlexLarsson <emilalexlarsson@gmail.com>
2026-09-10 10:23:53 +0200

    Implement leaderboard retrieval functionality in UserService and expose API endpoint in UserController

--- backend/TechLingo.Core/Interfaces/IUserService.cs
+++ backend/TechLingo.Core/Interfaces/IUserService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using TechLingo.Core.DTOs;
 
 namespace TechLingo.Core.Interfaces
@@ -9,5 +10,6 @@ namespace TechLingo.Core.Interfaces
     {
         Task<UserProfileDto?> GetUserByIdAsync(string userId);
         Task<bool> DeleteUserAsync(string userId);
+        Task<List<LeaderboardUserDto>> GetLeaderboardAsync();
     }
 }
